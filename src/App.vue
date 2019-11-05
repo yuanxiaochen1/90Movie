545460f6b810cffb0ad8b6da3a899c11757abef7
yuanxiaochen <492900476@qq.com>
2019-11-05 15:43:07 +0800

    等路页面完成-Yxc

--- src/App.vue
+++ src/App.vue
@@ -25,12 +25,20 @@ export default {
 </script>
 
 <style lang="less">
+html,
+body,
+#app {
+  height: 100%;
+  overflow: hidden;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
+  height: 100%;
 }
 
 #nav {
