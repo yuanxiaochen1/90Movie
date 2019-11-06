d939088413af97995ce668609675b73fa290eb5c
jinshixi <554242654@qq.com>
2019-11-06 12:57:27 +0800

    '热映第二次提交--jsx'

--- src/pages/Cinema/Re.vue
+++ src/pages/Cinema/Re.vue
@@ -10,9 +10,11 @@
           <li style="font-size:.25rem; margin-top:.2rem;color:#999;">2019-11-01</li>
           <li style="font-size:.25rem;color:#999;margin-top:.1rem">111分钟 - 爱情 | 动画 | 奇幻</li>
         </ul>
-        <div class="pf" >
-        <van-rate  v-model="value" readonly />
-        <li style=" display:block;list-style:none;font-size:.25rem;color:#999; margin-left:.2rem;margin-top:.05rem;"  >7.1分(豆瓣)</li>
+        <div class="pf">
+          <van-rate v-model="value" allow-half  readonly />
+          <li
+            style=" display:block;list-style:none;font-size:.25rem;color:#999; margin-left:.2rem;margin-top:.05rem;"
+          >7.1分(豆瓣)</li>
         </div>
       </div>
     </div>
@@ -27,9 +29,11 @@
           <li style="font-size:.25rem; margin-top:.2rem;color:#999;">2019-11-01</li>
           <li style="font-size:.25rem;color:#999;margin-top:.1rem">111分钟 - 爱情 | 动画 | 奇幻</li>
         </ul>
-        <div class="pf" >
-        <van-rate  v-model="value" readonly />
-        <li style=" display:block;list-style:none;font-size:.25rem;color:#999; margin-left:.2rem;margin-top:.05rem;"  >7.1分(豆瓣)</li>
+        <div class="pf">
+          <van-rate v-model="value" allow-half readonly />
+          <li
+            style=" display:block;list-style:none;font-size:.25rem;color:#999; margin-left:.2rem;margin-top:.05rem;"
+          >7.1分(豆瓣)</li>
         </div>
       </div>
     </div>
@@ -44,9 +48,11 @@
           <li style="font-size:.25rem; margin-top:.2rem;color:#999;">2019-11-01</li>
           <li style="font-size:.25rem;color:#999;margin-top:.1rem">111分钟 - 爱情 | 动画 | 奇幻</li>
         </ul>
-        <div class="pf" >
-        <van-rate  v-model="value" readonly />
-        <li style=" display:block;list-style:none;font-size:.25rem;color:#999; margin-left:.2rem;margin-top:.05rem;"  >7.1分(豆瓣)</li>
+        <div class="pf">
+          <van-rate v-model="value" allow-half readonly />
+          <li
+            style=" display:block;list-style:none;font-size:.25rem;color:#999; margin-left:.2rem;margin-top:.05rem;"
+          >7.1分(豆瓣)</li>
         </div>
       </div>
     </div>
@@ -61,9 +67,11 @@
           <li style="font-size:.25rem; margin-top:.2rem;color:#999;">2019-11-01</li>
           <li style="font-size:.25rem;color:#999;margin-top:.1rem">111分钟 - 爱情 | 动画 | 奇幻</li>
         </ul>
-        <div class="pf" >
-        <van-rate  v-model="value" readonly />
-        <li style=" display:block;list-style:none;font-size:.25rem;color:#999; margin-left:.2rem;margin-top:.05rem;"  >7.1分(豆瓣)</li>
+        <div class="pf">
+          <van-rate v-model="value" allow-half readonly />
+          <li
+            style=" display:block;list-style:none;font-size:.25rem;color:#999; margin-left:.2rem;margin-top:.05rem;"
+          >7.1分(豆瓣)</li>
         </div>
       </div>
     </div>
@@ -78,19 +86,39 @@
           <li style="font-size:.25rem; margin-top:.2rem;color:#999;">2019-11-01</li>
           <li style="font-size:.25rem;color:#999;margin-top:.1rem">111分钟 - 爱情 | 动画 | 奇幻</li>
         </ul>
-        <div class="pf" >
-        <van-rate  v-model="value" readonly />
-        <li style=" display:block;list-style:none;font-size:.25rem;color:#999; margin-left:.2rem;margin-top:.05rem;"  >7.1分(豆瓣)</li>
+        <div class="pf">
+          <van-rate v-model="value" allow-half readonly />
+          <li
+            style=" display:block;list-style:none;font-size:.25rem;color:#999; margin-left:.2rem;margin-top:.05rem;"
+          >7.1分(豆瓣)</li>
         </div>
       </div>
     </div>
-
+    <!-- 复制的内容 -->
+    <div class="re-container-a">
+      <div class="re-container-a-left">
+        <img src="../../assets/images/re-04.jpg" alt srcset style="width:100%;height:100%" />
+      </div>
+      <div class="re-container-a-right">
+        <ul>
+          <li style="display:block; width:100%;margin-left:-.7rem;;">少年的你</li>
+          <li style="font-size:.25rem; margin-top:.2rem;color:#999;">2019-11-01</li>
+          <li style="font-size:.25rem;color:#999;margin-top:.1rem">111分钟 - 爱情 | 动画 | 奇幻</li>
+        </ul>
+        <div class="pf">
+          <van-rate v-model="value" allow-half readonly />
+          <li
+            style=" display:block;list-style:none;font-size:.25rem;color:#999; margin-left:.2rem;margin-top:.05rem;"
+          >7.1分(豆瓣)</li>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {value:2.5};
   },
   components: {}
 };
@@ -99,11 +127,12 @@ export default {
 .re-container {
   width: 100%;
   height: 100%;
+  overflow: auto;
   .re-container-a {
     width: 100%;
     height: 2.5rem;
     background-color: white;
-    border-bottom: solid #999 .01rem;
+    border-bottom: solid rgba(116, 115, 115, 0.521) 0.01rem;
     display: flex;
     .re-container-a-left {
       width: 20%;
@@ -127,11 +156,11 @@ export default {
           margin-top: 0.3rem;
         }
       }
-      .pf{
+      .pf {
         width: 100%;
         height: 30%;
-        margin-top: -.6rem;
-        margin-left: .3rem;
+        margin-top: -0.6rem;
+        margin-left: 0.3rem;
         display: flex;
       }
     }
